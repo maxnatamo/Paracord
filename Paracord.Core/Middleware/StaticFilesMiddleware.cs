b065f00958fb54a95b5a95314131a2ac05ef4bf2
Max <madstk1@pm.me>
2023-04-11 10:28:47 +0200

    feat: Add initial static file support

---
+++ Paracord.Core/Middleware/StaticFilesMiddleware.cs
@@ -0,0 +1,49 @@
+using Paracord.Core.Http;
+using Paracord.Core.Listener;
+using Paracord.Shared.Models;
+
+namespace Paracord.Core.Middleware
+{
+    public class StaticFilesOptions
+    {
+        /// <summary>
+        /// The path for the static content, relative to the executing binary.
+        /// </summary>
+        public string ContentPath { get; set; } = "wwwroot";
+    }
+
+    /// <summary>
+    /// This middleware handles static files that might be present on the server.
+    /// </summary>
+    public class StaticFilesMiddleware : MiddlewareBase
+    {
+        /// <inheritdoc cref="StaticFilesOptions.ContentPath" />
+        public readonly string ContentPath = "wwwroot";
+
+        public StaticFilesMiddleware()
+        { }
+
+        public StaticFilesMiddleware(StaticFilesOptions options = default!)
+        {
+            this.ContentPath = options.ContentPath;
+        }
+
+        public override void AfterRequestReceived(HttpListener listener, HttpRequest request, HttpResponse response)
+        {
+            string localPath = request.Path.StartsWith("/") ? request.Path.Substring(1) : request.Path;
+            string absoluteRequestPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ContentPath, localPath);
+
+            if(!File.Exists(absoluteRequestPath))
+            {
+                this.Next();
+                return;
+            }
+
+            byte[] fileContent = File.ReadAllBytes(absoluteRequestPath);
+
+            response.Headers["Content-Type"] = MimeTypes.ResolveMimeType(localPath);
+            response.Write(fileContent, 0, fileContent.Length);
+            response.Send();
+        }
+    }
+}
