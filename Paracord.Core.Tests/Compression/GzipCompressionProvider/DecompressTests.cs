671f08ceb65d78687e4af31adaacaf9494d3ee22
Max <madstk1@pm.me>
2023-04-12 15:53:28 +0200

    test: Add tests for GzipCompressionProvider

---
+++ Paracord.Core.Tests/Compression/GzipCompressionProvider/DecompressTests.cs
@@ -0,0 +1,38 @@
+using System.Text;
+
+using Paracord.Core.Compression;
+
+namespace Paracord.Core.Tests.Compression.GzipCompressionProviderTests
+{
+    public class DecompressTests
+    {
+        [Fact]
+        public void DecompressReturnsEmptyArrayGivenEmptyStream()
+        {
+            // Arrange
+            MemoryStream inputStream = new MemoryStream();
+
+            // Act
+            byte[] decompressed = new GzipCompressionProvider().Decompress(inputStream);
+
+            // Assert
+            decompressed.Length.Should().Be(0);
+        }
+
+        [Fact]
+        public void DecompressReturnsSameAsInputGivenStringContent()
+        {
+            // Arrange
+            string content = "This is a test string!";
+
+            MemoryStream inputStream = new MemoryStream(Encoding.ASCII.GetBytes(content));
+
+            // Act
+            byte[] compressed = new GzipCompressionProvider().Compress(new MemoryStream(Encoding.ASCII.GetBytes(content)));
+            byte[] decompressed = new GzipCompressionProvider().Decompress(new MemoryStream(compressed));
+
+            // Assert
+            Encoding.ASCII.GetString(decompressed.ToArray()).Should().Be(content);
+        }
+    }
+}
