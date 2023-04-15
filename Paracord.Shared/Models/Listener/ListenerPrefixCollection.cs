d2d5cb15247f582702dc39ba2ea5426847c2f614
Max <madstk1@pm.me>
2023-04-15 13:53:41 +0200

    feat(http): Add 'Add' extensions for ListenerPrefixCollection

--- Paracord.Shared/Models/Listener/ListenerPrefixCollection.cs
+++ Paracord.Shared/Models/Listener/ListenerPrefixCollection.cs
@@ -7,6 +7,24 @@ namespace Paracord.Shared.Models.Listener
     /// </summary>
     public class ListenerPrefixCollection : Collection<ListenerPrefix>
     {
+        /// <summary>
+        /// Adds a new prefix to the collection, with the specified address.
+        /// </summary>
+        /// <param name="address">The address of the prefix-instance.</param>
+        public void Add(string address)
+        {
+            this.Add(ListenerPrefix.Parse(address));
+        }
 
+        /// <summary>
+        /// Adds a new prefix to the collection, with the specified values.
+        /// </summary>
+        /// <param name="address">The IP address of the prefix-instance.</param>
+        /// <param name="port">The port of the prefix-instance.</param>
+        /// <param name="protocol">The protocol of the prefix-instance.</param>
+        public void Add(string address, uint port = 80, string protocol = "http")
+        {
+            this.Add(new ListenerPrefix(address, port, protocol));
+        }
     }
 }
